1e251f7c990928eb01a591fc2585e829e025af35
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-05-12 19:45:42 -0400

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+@file:Suppress("SpellCheckingInspection")
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.31"
+    `maven-publish`
+}
+
+group = "ca.warp7.rt.router"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    // Kotlin Coroutines
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.2.1")
+    // HTTP Requests Library
+    implementation(group = "com.github.kittinunf.fuel", name = "fuel", version = "2.0.1")
+    testImplementation(kotlin("test"))
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    repositories {
+        maven {
+            // change to point to your repo, e.g. http://my.org/repo
+            url = uri("$buildDir/repo")
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+            artifactId = "rt-router"
+        }
+    }
+}
