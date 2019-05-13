422528da83191e854abe30ab84d27a650b5d9eb9
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-05-12 20:21:20 -0400

    add .travis.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.2.1")
     // HTTP Requests Library
     implementation(group = "com.github.kittinunf.fuel", name = "fuel", version = "2.0.1")
+    // JSON Library
+    implementation(group = "com.beust", name = "klaxon", version = "5.0.5")
     testImplementation(kotlin("test"))
 }
 
