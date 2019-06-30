6729056741c68c9ec72b0b1f0c7959326a5f836b
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-06-29 21:26:03 -0400

    bump Kotlin version to 1.3.40

--- src/main/kotlin/ca/warp7/rt/router/tba/Client.kt
+++ src/main/kotlin/ca/warp7/rt/router/tba/Client.kt
@@ -9,9 +9,10 @@ import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.coroutines.awaitString
-import java.io.StringReader
 
 private var authKey = ConfigSystem.getTBAKey()
+private var loadingCache = false
+private var savingCache = false
 
 fun TBA.hasKey() = authKey == null
 
@@ -19,12 +20,20 @@ fun TBA.setKey(key: String) {
     authKey = key
 }
 
+fun TBA.setOptions(
+    loadCache: Boolean = true,
+    saveCache: Boolean = true
+) {
+    loadingCache = loadCache
+    savingCache = saveCache
+}
+
 private suspend fun TBA.getParsed(requestURL: String): JsonBase {
     val response = Fuel
         .get("http://www.thebluealliance.com/api/v3$requestURL")
         .header("X-TBA-Auth-Key" to authKey!!, "User-Agent" to ConfigSystem.getUserAgent())
         .awaitString()
-    return Parser.default().parse(StringReader(response)) as JsonBase
+    return Parser.default().parse(StringBuilder(response)) as JsonBase
 }
 
 internal suspend fun TBA.get(requestURL: String): JsonObject {
